827c3c7bf6cf9de3229ada97fb4dccae5b7a4cad
n0bisuke <rsksound@gmail.com>
2023-02-27 09:38:12 +0900

    タイトル変えてみる

--- app/server-constants.js
+++ app/server-constants.js
@@ -5,8 +5,8 @@ const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
-const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
+const NEXT_PUBLIC_SITE_TITLE = 'ハッカワズブログ'
+const NEXT_PUBLIC_SITE_DESCRIPTION = 'シビックテック領域で活躍する人をメインに紹介していく予定です。CivicTech LTとは、市民技術コミュニティのLightning Talk(ライトニングトーク)イベントの略称です。Lightning Talkとは、5分以内で1つのテーマを簡潔に発表する形式のイベントであり、CivicTech LTではこれまで市民技術分野で活躍するスピーカーによるデータ分析や機械学習、人工知能(AI)に関するLTが行われています。'
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
